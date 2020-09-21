b032e8a03e9fc4a6ab8e64c1b44f705cc0199f40
alvin almodal <almodalalvin@gmail.com>
2020-09-21 22:09:11 +0800

    added IsApproved property in Photo Class

---
+++ Migrations/20200921140530_PhotoIsApproved.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class PhotoIsApproved : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_RoleId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "Photos",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "Photos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
